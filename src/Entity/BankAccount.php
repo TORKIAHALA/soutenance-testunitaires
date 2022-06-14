a2b7098ab68e7233e140904c76ff9508405ed663
jeremdh62 <jdhaille@myges.fr>
2022-06-14 15:44:03 +0200

    add entity

---
+++ src/Entity/BankAccount.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BankAccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BankAccountRepository::class)]
+class BankAccount
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'float')]
+    private $balance;
+
+    #[ORM\Column(type: 'string', length: 15)]
+    private $RIB;
+
+    #[ORM\OneToOne(inversedBy: 'bankAccount', targetEntity: User::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private $owner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBalance(): ?float
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(float $balance): self
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getRIB(): ?string
+    {
+        return $this->RIB;
+    }
+
+    public function setRIB(string $RIB): self
+    {
+        $this->RIB = $RIB;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(User $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
