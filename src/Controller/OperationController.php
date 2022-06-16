84185095651261b0a279d544379b1cd4631052a5
jeremdh62 <jdhaille@myges.fr>
2022-06-16 23:26:03 +0100

    add send email

--- src/Controller/OperationController.php
+++ src/Controller/OperationController.php
@@ -4,15 +4,26 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\DepositFormType;
+use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OperationController extends AbstractController
 {
+
+    private EmailVerifier $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
@@ -31,12 +42,16 @@ class OperationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $em->getRepository(User::class)->find($request->get('user'));
 
-            if ($user->getBankAccount()->balanceValid($form->getData()['amount'])) {
-                $user->getBankAccount()->deposit($form->getData()['amount']);
+            $user->getBankAccount()->deposit($form->getData()['amount']);
 
-                $em->persist($user);
-                $em->flush();
-            };
+            if ($user->getBankAccount()->sendEmail(new \DateTime())) {
+                $this->sendEmail($user, 'account_deposit');
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('operation/deposit.html.twig', [
@@ -47,10 +62,46 @@ class OperationController extends AbstractController
     }
 
     #[Route('/withdraw', name: 'account_withdraw')]
-    public function withdraw(): Response
+    public function withdraw(Request $request, EntityManagerInterface $em): Response
     {
+
+
+        $form = $this->createForm(DepositFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $em->getRepository(User::class)->find($request->get('user'));
+
+            $user->getBankAccount()->withdraw($form->getData()['amount']);
+
+            if ($user->getBankAccount()->sendEmail(new \DateTime())) {
+                $this->sendEmail($user, 'account_withdraw');
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+
         return $this->render('operation/withdraw.html.twig', [
             'controller_name' => 'OperationController',
+            'form' => $form->createView(),
         ]);
+    }
+
+
+    private function sendEmail(User $user, string $route)
+    {
+        $this->emailVerifier->sendEmailConfirmation(
+            $route,
+            $user,
+            (new TemplatedEmail())
+                ->from(new Address('noreply@bankaccount.com', 'Bank Acccount'))
+                ->to($user->getEmail())
+                ->subject('Operation confirmer')
+                ->htmlTemplate('mailer/contentEmail.html.twig')
+        );
     }
 }
